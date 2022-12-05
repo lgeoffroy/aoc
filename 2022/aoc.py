29a5fba614018e914e4b39cc0a54767d8e9a8cd4
ludovic geoffroy <ludovic.geoffroy@thetribe.io>
2022-12-05 13:51:52 +0100

    day 4

--- 2022/aoc.py
+++ 2022/aoc.py
@@ -10,6 +10,7 @@ import timeit
 from day1 import solve as solve_day1
 from day2 import solve as solve_day2
 from day3 import solve as solve_day3
+from day4 import solve as solve_day4
 
 
 load_dotenv()
---
+++ 2022/day4.py
@@ -0,0 +1,12 @@
+def solve(lines):
+    total_lvl1 = 0
+    total_lvl2 = 0
+    for line in lines:
+        [pair1, pair2] = line.split(",")
+        [a, b] = map(lambda x: int(x), pair1.split("-"))
+        [c, d] = map(lambda x: int(x), pair2.split("-"))
+        if c <= a <= d and c <= b <= d or a <= c <= b and a <= d <= b:
+            total_lvl1 += 1
+        if c <= a <= d or c <= b <= d or a <= c <= b or a <= d <= b:
+            total_lvl2 += 1
+    return total_lvl1, total_lvl2
